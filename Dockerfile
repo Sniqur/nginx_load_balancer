09abd5e1d9858a6692d50af6dfc68fe25e9ae2b3
Sniqur <stephansnigur@gmail.com>
2025-02-08 22:55:18 +0200

    init commit with http

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:14
+
+WORKDIR /app
+
+COPY server.js .
+COPY *.html .
+COPY images ./images
+COPY css ./css
+COPY js ./js
+COPY package.json .
+
+RUN npm install
+
+EXPOSE 3000
+
+CMD ["node", "server.js"]
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const appName = process.env.APP_NAME
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+    console.log(`Request served by ${appName}`);
+});
+
+
+
+app.listen(port, () => {
+    console.log(`${appName} is listening on port ${port}`);
+});
